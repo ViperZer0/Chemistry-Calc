425a106802a6fba9162a14f1d760d0ba18437948
ViperZer0 <viperzer0thehackingreptile@gmail.com>
2016-12-15 23:46:57 -0900

    Last minute addition

--- createTable.c
+++ createTable.c
@@ -1,6 +1,12 @@
 /*Name: createTable.c
  *Contains: createTable()
  *General Description: Functions relating to the creation of the periodic table
+ *Notes: 
+
+ ***12/15/2016***
+Somehow we are getting a totally empty periodic table later on in the program. This function is PROBABLY the cause of it. It may be a Win-to-Linux issue, or it could be something
+that's just gone unnoticed for a while. If you're wondering why on earth NOTHING in the program is working, THIS IS WHY. THERE IS NO PERIODIC TABLE. Actually, there is, but it's all zeros. 
+Check the bookmark (//ISSUE) for a possible source of this issue.
  */ 
  
 
@@ -29,7 +35,7 @@ struct periodic *createTable(){
 	int i,j, o;
 	for(i=0;i<num_elements;i++){//Repeat for all the elements of the periodic table.
 		if(fgets(buff,200,fp)){
-			o=1;//Move all the characters from buff into strcopy, inserting a 0 between two consecutive tabs (\t\t)
+			o=1;//Move all the characters from buff into strcopy, inserting a 0 between two consecutive tabs (\t\t) //ISSUE
 			for(j = 0; j < 200; j++){
 				if (j > 0){
 					if (buff[j] == '\t' && buff[j-1] == '\t' ){
